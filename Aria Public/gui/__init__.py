bfa1b0cab6cd2eac9050fcc77dd463480da5f78f
VocalOpal <justofficialopals@gmail.com>
2025-10-02 17:59:40 -0400

    Delete Aria Public directory

--- Aria Public/gui/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Aria Voice Studio - Public Beta (v5) - Modern GUI Implementation
-Complete redesign with gradient-driven aesthetic
-"""
-
-from .main_window import AriaMainWindow, launch_gui
-
-__all__ = ['AriaMainWindow', 'launch_gui']
